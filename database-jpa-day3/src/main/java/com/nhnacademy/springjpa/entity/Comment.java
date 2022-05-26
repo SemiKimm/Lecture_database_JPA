693fd9fb6b813fb13324570c09c145d9b92de3ee
김세미 <semikimm.m@gmail.com>
2022-05-26 17:52:25 +0900

    [jpa-day3-hw] 2일차 commentTest 수정

--- database-jpa-day3/src/main/java/com/nhnacademy/springjpa/entity/Comment.java
+++ database-jpa-day3/src/main/java/com/nhnacademy/springjpa/entity/Comment.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.springjpa.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -26,11 +27,20 @@ public class Comment {
     @Column(name = "comment_content", nullable = false)
     private String content;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
     @JoinColumn(name = "post_no", nullable = false)
     private Post post;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
     @JoinColumn(name = "user_no", nullable = false)
     private User user;
+
+    public static Comment create(String content, User user, Post post){
+        Comment comment = new Comment();
+        comment.setContent(content);
+        comment.setUser(user);
+        comment.setPost(post);
+
+        return comment;
+    }
 }
--- database-jpa-day3/src/test/java/com/nhnacademy/springjpa/entity/CommentTest.java
+++ database-jpa-day3/src/test/java/com/nhnacademy/springjpa/entity/CommentTest.java
@@ -1,6 +1,5 @@
 package com.nhnacademy.springjpa.entity;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.nhnacademy.springjpa.config.RootConfig;
@@ -10,8 +9,6 @@ import com.nhnacademy.springjpa.repository.CommentRepository;
 import com.nhnacademy.springjpa.repository.PostRepository;
 import com.nhnacademy.springjpa.repository.UserRepository;
 import com.nhnacademy.springjpa.repository.UserTypeRepository;
-import java.sql.Timestamp;
-import java.util.Date;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,56 +27,28 @@ import org.springframework.transaction.annotation.Transactional;
 })
 class CommentTest {
     @Autowired
-    UserTypeRepository userTypeRepository;
-
-    @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    PostRepository postRepository;
-
-    @Autowired
-    BoardTypeRepository boardTypeRepository;
-
-    @Autowired
     CommentRepository commentRepository;
 
     @Test
     public void testCommentEntity(){
         UserType userType = new UserType();
-        userType.setCode(2);
         userType.setValue("일반");
-        userTypeRepository.save(userType);
 
-        User user1 = new User();
-        user1.setId("user1");
-        user1.setNickname("사용자");
-        user1.setPassword("1234");
-        user1.setType(userType);
-        userRepository.save(user1);
+        User user1 = User.create("user1", "1234", "사용자", userType);
 
         BoardType boardType1 = new BoardType();
-        boardType1.setCode(1);
         boardType1.setValue("일반게시판");
-        boardTypeRepository.save(boardType1);
 
-        Post post1 = new Post();
-        post1.setTitle("hello");
-        post1.setContent("hi");
-        post1.setWriteDateTime(new Timestamp(new Date().getTime()));
-        post1.setUser(user1);
-        post1.setReplyOrder(0);
-        post1.setType(boardType1);
-        post1.setDeleteFlag(false);
-        postRepository.save(post1);
+        Post post1 = Post.create("제목", "내용", user1, boardType1);
 
-        Comment comment = new Comment();
-        comment.setContent("안녕");
-        comment.setPost(post1);
-        comment.setUser(user1);
-        commentRepository.save(comment);
+        Comment comment = Comment.create("내용", user1, post1);
+        commentRepository.saveAndFlush(comment);
 
-        assertThat(comment.getUser().getId()).isEqualTo(user1.getId());
-        assertThat(comment.getPost().getNo()).isEqualTo(post1.getNo());
+        Comment result = commentRepository.findById(comment.getNo()).orElse(null);
+        assertThat(result).isNotNull();
+        assertThat(result.getNo()).isEqualTo(comment.getNo());
+        assertThat(result.getContent()).isEqualTo(comment.getContent());
+        assertThat(result.getUser().getNo()).isEqualTo(user1.getNo());
+        assertThat(result.getPost().getNo()).isEqualTo(post1.getNo());
     }
 }
