8499eae70bf26185a573d1b381768ea74d820b26
김세미 <semikimm.m@gmail.com>
2022-05-26 18:47:37 +0900

    [jpa-day3-hw] 2일차 repository interface 작성시 메서드 이름으로 쿼리 생성 과제 제대로 다시 - PostRepository 완료

--- database-jpa-day3/src/main/java/com/nhnacademy/springjpa/repository/PostRepository.java
+++ database-jpa-day3/src/main/java/com/nhnacademy/springjpa/repository/PostRepository.java
@@ -2,9 +2,12 @@ package com.nhnacademy.springjpa.repository;
 
 import com.nhnacademy.springjpa.domain.PostDto;
 import com.nhnacademy.springjpa.entity.Post;
+import com.nhnacademy.springjpa.entity.User;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
     List<PostDto> findAllByDeleteFlag(boolean deleteFlag);
+    List<PostDto> findAllByTitleContaining(String searchKeyword);
+    List<PostDto> findAllByUser(User user);
 }
--- database-jpa-day3/src/test/java/com/nhnacademy/springjpa/repository/PostRepositoryTest.java
+++ database-jpa-day3/src/test/java/com/nhnacademy/springjpa/repository/PostRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.nhnacademy.springjpa.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.nhnacademy.springjpa.config.RootConfig;
 import com.nhnacademy.springjpa.config.WebConfig;
+import com.nhnacademy.springjpa.domain.PostDto;
 import com.nhnacademy.springjpa.entity.Post;
-import com.nhnacademy.springjpa.entity.User;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,13 +24,38 @@ import org.springframework.transaction.annotation.Transactional;
     @ContextConfiguration(classes = WebConfig.class)
 })
 class PostRepositoryTest {
-
     @Autowired
     PostRepository postRepository;
+    @Autowired
+    UserRepository userRepository;
 
-//    @Test
-//    void findAllBy() {
-//        User user = User
-//        Post post = Post.create("제모오오옥", "내요오옹", )
-//    }
+    @Test
+    void findAllBy() {
+        List<Post> result = postRepository.findAll();
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @Test
+    void findAllByDeleteFlag_isFalse(){
+        List<PostDto> result = postRepository.findAllByDeleteFlag(false);
+        assertThat(result).isNotEmpty().hasSize(2);
+    }
+
+    @Test
+    void findAllByDeleteFlag_isTrue(){
+        List<PostDto> result = postRepository.findAllByDeleteFlag(true);
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void findAllByTitleContaining(){
+        List<PostDto> result = postRepository.findAllByTitleContaining("안");
+        assertThat(result).isNotEmpty().hasSize(1);
+    }
+
+    @Test
+    void findAllByUser(){
+        List<PostDto> result = postRepository.findAllByUser(userRepository.findById(1).orElse(null));
+        assertThat(result).isNotEmpty().hasSize(3);
+    }
 }
